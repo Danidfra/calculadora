d8b7d701b6bffde8e465d3c7c1db30532a9aae17
Daniel Filemon <danielfilemondev@gmail.com>
2024-05-04 23:43:42 -0300

    Criado modulo para armazenar as variaveis

---
+++ variaveis/variaveis.py
@@ -0,0 +1,32 @@
+# armazena o numero do mostradpr
+nmr_mostrador = ["0"]
+# armazena o número para realizar as operações
+# nmr_para_operar = ["0"]
+# armazena o resultado da operação
+resultado = []
+# armazena a conta da operação
+conta= []
+status_comma = [False]
+
+# nmr_mostrador_formatado = testaNmr(nmr_mostrador)
+# nmr_mostrador_formatado = formata_nmr(nmr_mostrador)
+
+nmr0 = "0"
+nmr1 = "1"
+nmr2 = "2"
+nmr3 = "3"
+nmr4 = "4"
+nmr5 = "5"
+nmr6 = "6"
+nmr7 = "7"
+nmr8 = "8"
+nmr9 = "9"
+sim_plus = "+"
+sim_minus = "-"
+sim_times = "x"
+sim_div = "÷"
+sim_equals = "="
+comma = ','
+porc = '%'
+invert = '+/-'
+clear = 'C'
